37a1d39b2c4f0339732471d5cf05991eeb9bc403
oscar <pochiachen0321@gmail.com>
2023-01-06 16:25:55 +0800

    fish: add shell config

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+# editor setup
+set -gx EDITOR nvim
+
+# starship setup
+set -gx STARSHIP_CONFIG "$HOME/.config/starship.toml"
+set -gx STARSHIP_CACHE "$HOME/.starship/cache"
+
+# alias setup
+alias vim="nvim"
+alias vi="nvim"
+alias lg="lazygit"
+alias tm="tmux"
+alias tma="tmux at"
+alias tmt="tmux at -t"
+alias tmk="tmux kill-session -t"
+alias tml="tmux ls"
+alias ls="exa -lag --header"
+
+# colored man setup
+set -g man_blink -o red
+set -g man_bold -o green
+set -g man_standout -b black 93a1a1
+set -g man_underline -u 93a1a1
