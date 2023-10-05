6cf5e41358968bbe2ad67d0f78a81529ea456887
kashiwachen <pochiachen0321@gmail.com>
2023-10-05 22:35:20 +0800

    core: script to deploy on new machine

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# TODO: Not test yet
+# sync nvim config
+cp -r ./nvim $HOME/.config/nvim
+# The followings are TODOs
+# sync alacritty config
+cp -r ./alacritty $HOME/.config/alacritty
+# sync starship config
+cp ./starship.toml $HOME/.config/starship.toml
+# sync fish shell config
+cp -r ./fish $HOME/.config/fish 
+# sync git config
+cp -r ./git $HOME/.config
+# sync neofetch config
+cp -r ./neofetch $HOME/.config/neofetch
+# sync tmux config
+cp -r ./tmux $HOME
