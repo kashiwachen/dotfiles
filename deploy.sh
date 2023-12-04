49e3d694b9bf7161120fc59f38cc6ac83fb44a6e
oscar <oscar.chen@flywheel.jp>
2023-12-04 17:40:14 +0900

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 # TODO: Not test yet
 # sync nvim config
-cp -r ./nvim $HOME/.config/nvim
+cp -r ./nvim $HOME/.config
 # The followings are TODOs
 # sync alacritty config
-cp -r ./alacritty $HOME/.config/alacritty
+cp -r ./alacritty $HOME/.config
 # sync starship config
 cp ./starship.toml $HOME/.config/starship.toml
 # sync fish shell config
-cp -r ./fish $HOME/.config/fish 
+cp -r ./fish $HOME/.config
 # sync git config
 cp -r ./git $HOME/.config
 # sync neofetch config
-cp -r ./neofetch $HOME/.config/neofetch
+cp -r ./neofetch $HOME/.config
 # sync tmux config
 cp -r ./tmux/tmux.conf $HOME/.tmux.conf
+# sync common used scripts
+cp -r ./bin $HOME
