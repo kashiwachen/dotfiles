46b7ddb2de8455d7d5872bcb6bca652994e8a05e
kashiwachen <oscar.chen@flywheel.jp>
2023-12-06 13:24:27 +0900

    deploy: add TODOs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
+# Dependancies
+# TODO: install homebrew
+# TODO: install neovim
+# TODO: install terminal(alacritty/kitty)
+# TODO: install CLI shell(fish)
+# TODO: install shell prompt(starship)
+# TODO: install tmux
+# TODO: install 
+
 # TODO: Not test yet
 # sync nvim config
 cp -r ./nvim $HOME/.config
@@ -10,9 +19,13 @@ cp ./starship.toml $HOME/.config/starship.toml
 cp -r ./fish $HOME/.config
 # sync git config
 cp -r ./git/.git* $HOME
+# sync a git diff script
 cp -r ./bin $HOME
+# Sync CLI tools
 # sync neofetch config
 cp -r ./neofetch $HOME/.config
+# TODO: install z jump
+# TODO: install fzf -> fuzzy search
 # sync tmux config
 cp -r ./tmux/tmux.conf $HOME/.tmux.conf
 # sync common used scripts
