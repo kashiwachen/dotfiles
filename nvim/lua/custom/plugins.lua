40d93b2ff54bf5d1469ad814170d537906c31f39
kashiwachen <oscar.chen@flywheel.jp>
2024-03-28 10:06:09 +0900

    nvim: update for rust

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -3,7 +3,8 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
-        "pyright"
+        "pyright",
+        "rust-analyzer"
       },
     },
   },
@@ -12,6 +13,24 @@ local plugins = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    configs = function (_, opts)
+      require("rust-tools").setup(opts)
     end,
   },
 }
