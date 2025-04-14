1921e9d834f94a4602828078ce40705c14dc6d8c
kashiwachen <oscar.chen@flywheel.jp>
2025-04-14 23:26:40 +0900

    [BIG CHANGE] complete setup.sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,78 @@
-# setup
-# Install homebrew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# !/usr/bin/env bash
+set -euo pipefail -o posix
 
-# Install starship
-# curl -sS https://starship.rs/install.sh | sh
+configure_zsh() {
+  # shell config
+  if [[ "$SHELL" != "$(which zsh)" ]]; then
+    echo "> Changing default shell to Zsh..."
+    chsh -s "$(which zsh)"
+  else
+    echo "> Zsh is already the default shell."
+  fi
+
+  # zsh config
+  if ! grep -q "source $HOME/.config/zsh/config.zsh" $HOME/.zshrc; then
+    echo "source $HOME/.config/zsh/config.zsh" >>$HOME/.zshrc
+    echo "> Load myown config.zsh in zsh shell"
+  else
+    echo "> Already loaded my own config.zsh"
+  fi
+}
+
+configure_starship() {
+  if ! grep -q 'eval "$(starship init zsh)"' $HOME/.zshrc; then
+    echo '# Load starship prompt' >>$HOME/.zshrc
+    echo 'eval "$(starship init zsh)"' >>$HOME/.zshrc
+    echo "> Added starship init to .zshrc"
+  fi
+}
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install tools
-# brew install neovim yazi tmux ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide imagemagick font-symbols-only-nerd-font
+brew install \
+  neovim yazi tmux eza lazygit \
+  starship fzf z \
+  ffmpeg sevenzip jq poppler fd ripgrep \
+  zoxide imagemagick \
+  font-symbols-only-nerd-font
+
+# Install rectangle
+brew install --cask \
+  rectangle raycast 1password wezterm \
+  arc firefox readdle-spark \
+  notion notion-calendar \
+  spotify
 
 # Install z jump
-# git clone https://github.com/agkozak/zsh-z.git $HOME/.config/zsh.configs/
-# end setup
+git clone https://github.com/agkozak/zsh-z.git $HOME/.config/zsh.configs/zsh-z/
 
 # Deploy config and scrips
-# nvim config
+echo '> Start to copy the configurations'
+
+## nvim config
 cp -r ./nvim $HOME/.config
-# starship config
-cp ./starship.toml $HOME/.config/starship.toml
-# fish shell config
-cp -r ./fish $HOME/.config
-# git config
+## starship config
+cp ./starship.toml ~/.config/starship.toml
+## git config
 cp -r ./git/.git* $HOME
-# a git diff script
+## a git diff script
 cp -r ./bin $HOME
-# CLI tools
-# neofetch config
+## neofetch config
 cp -r ./neofetch $HOME/.config
-# tmux config
+## tmux config
 cp -r ./tmux/tmux.conf $HOME/.tmux.conf
-# TODO: install fzf -> fuzzy search
-# TODO: deploy yazi config
+## yazi config
 cp -r ./yazi $HOME/.config
+
+echo "> link starship.toml to dotfiles"
+ln -sfn $PWD/starship.toml ~/.config/starship.toml
+
+mkdir $HOME/.config/zsh
+cp $PWD/zsh/config.zsh $HOME/.config/zsh/config.zsh
+echo "> link cofig.zsh to dotfiles"
+ln -sfn $PWD/zsh/config.zsh $HOME/.config/zsh/config.zsh
+
+configure_zsh
+configure_starsh
